b1fbbd752a94c70f50f55df6f8023741456c6cce
itsAbhirajSinha <mailabhiraj123@gmail.com>
2024-03-24 12:27:00 +0530

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -4,20 +4,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // for more information about configuring your Middleware
  
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
-  // Prevent the specified routes from accessing
-  // authentication information:
-  // ignoredRoutes: ['/no-auth-in-this-route'],
+  publicRoutes: ['/api/webhooks/clerk']
 });
  
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
- 
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"
   ]
 };
